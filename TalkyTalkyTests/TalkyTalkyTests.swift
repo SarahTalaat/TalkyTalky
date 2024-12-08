6e7c32516538d20343aaa46fb2f7e478db56e0f8
SarahTalaat <sarahtalaatammar.work@gmail.com>
2024-12-08 03:26:10 +0200

    Initial Commit

---
+++ TalkyTalkyTests/TalkyTalkyTests.swift
@@ -0,0 +1,17 @@
+//
+//  TalkyTalkyTests.swift
+//  TalkyTalkyTests
+//
+//  Created by Sara Talat on 08/12/2024.
+//
+
+import Testing
+@testable import TalkyTalky
+
+struct TalkyTalkyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ TalkyTalkyUITests/TalkyTalkyUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  TalkyTalkyUITestsLaunchTests.swift
+//  TalkyTalkyUITests
+//
+//  Created by Sara Talat on 08/12/2024.
+//
+
+import XCTest
+
+final class TalkyTalkyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
